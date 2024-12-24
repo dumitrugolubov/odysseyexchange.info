6a6639ffe69bfc6d09f7857c32e75c53c8ee1bd3
dumitrugolubov <107499940+dumitrugolubov@users.noreply.github.com>
2024-12-24 13:41:52 +0200

    Update vite.config.ts

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: 'https://odyssey.exchange',
+        origin: 'https://odysseyexchange.info',
       }),
     ],
   };
